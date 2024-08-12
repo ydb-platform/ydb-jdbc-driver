4e581be342f67bab386a35d51c342c837f959fd3
Alexandr Gorshenin <alexandr268@ydb.tech>
2024-08-12 18:30:52 +0100

    Added collection of stats to YdbContext

--- jdbc/src/main/java/tech/ydb/jdbc/context/YdbContext.java
+++ jdbc/src/main/java/tech/ydb/jdbc/context/YdbContext.java
@@ -39,8 +39,10 @@ import tech.ydb.table.TableClient;
 import tech.ydb.table.description.TableColumn;
 import tech.ydb.table.description.TableDescription;
 import tech.ydb.table.impl.PooledTableClient;
+import tech.ydb.table.query.ExplainDataQueryResult;
 import tech.ydb.table.rpc.grpc.GrpcTableRpc;
 import tech.ydb.table.settings.DescribeTableSettings;
+import tech.ydb.table.settings.ExplainDataQuerySettings;
 import tech.ydb.table.settings.PrepareDataQuerySettings;
 import tech.ydb.table.settings.RequestSettings;
 import tech.ydb.table.values.Type;
@@ -68,6 +70,7 @@ public class YdbContext implements AutoCloseable {
     private final SessionRetryContext retryCtx;
 
     private final Cache<String, YdbQuery> queriesCache;
+    private final Cache<String, QueryStat> queryStatesCache;
     private final Cache<String, Map<String, Type>> queryParamsCache;
 
     private final boolean autoResizeSessionPool;
@@ -98,8 +101,14 @@ public class YdbContext implements AutoCloseable {
         if (cacheSize > 0) {
             queriesCache = CacheBuilder.newBuilder().maximumSize(cacheSize).build();
             queryParamsCache = CacheBuilder.newBuilder().maximumSize(cacheSize).build();
+            if (config.isFullScanDetectorEnabled()) {
+                queryStatesCache = CacheBuilder.newBuilder().maximumSize(cacheSize).build();
+            } else {
+                queryStatesCache = null;
+            }
         } else {
             queriesCache = null;
+            queryStatesCache = null;
             queryParamsCache = null;
         }
     }
@@ -270,6 +279,27 @@ public class YdbContext implements AutoCloseable {
         if (cached == null) {
             cached = parseYdbQuery(sql);
             queriesCache.put(sql, cached);
+
+            if (queryStatesCache != null) {
+                QueryStat stat = queryStatesCache.getIfPresent(sql);
+                if (stat == null) {
+                    final String preparedYQL = cached.getPreparedYql();
+                    final ExplainDataQuerySettings settings = withDefaultTimeout(new ExplainDataQuerySettings());
+                    Result<ExplainDataQueryResult> res = retryCtx.supplyResult(
+                            session -> session.explainDataQuery(preparedYQL, settings)
+                    ).join();
+
+                    if (res.isSuccess()) {
+                        ExplainDataQueryResult exp = res.getValue();
+                        stat = new QueryStat(cached, exp.getQueryAst(), exp.getQueryPlan());
+                    } else {
+                        stat = new QueryStat(cached, res.getStatus());
+                    }
+                    queryStatesCache.put(sql, stat);
+                }
+
+                stat.incrementUsage();
+            }
         }
 
         return cached;
@@ -288,8 +318,8 @@ public class YdbContext implements AutoCloseable {
                 ).join();
 
                 if (result.isSuccess()) {
-                    TableDescription d = result.getValue();
-                    types = result.getValue().getColumns().stream()
+                    TableDescription descrtiption = result.getValue();
+                    types = descrtiption.getColumns().stream()
                             .collect(Collectors.toMap(TableColumn::getName, TableColumn::getType));
                     queryParamsCache.put(query.getOriginQuery(), types);
                 }
