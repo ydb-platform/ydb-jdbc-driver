492ca8e797d58f68eab12750067d497655f299c0
Alexandr Gorshenin <alexandr268@ydb.tech>
2026-07-02 16:04:00 +0100

    Fixed javadoc warnings

--- jdbc/src/main/java/tech/ydb/jdbc/YdbResultSetMetaData.java
+++ jdbc/src/main/java/tech/ydb/jdbc/YdbResultSetMetaData.java
@@ -10,9 +10,9 @@ public interface YdbResultSetMetaData extends ResultSetMetaData {
     /**
      * Returns native YDB type for column
      *
-     * @param column column, 1..N
+     * @param column column index, 1..N
      * @return YDB type
-     * @throws java.sql.SQLException
+     * @throws java.sql.SQLException if result set doesn't have this index
      */
     Type getYdbType(int column) throws SQLException;
 }
